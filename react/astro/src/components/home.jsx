221960c779456e0ffa6593ed7db9864dea4e9d2e
unknown <amanmorghade747392@gmail.com>
2025-02-07 18:29:17 +0530

    Initial commit

---
+++ react/astro/src/components/home.jsx
@@ -0,0 +1,14 @@
+function Home(){
+    return <header id="home">
+      <div className="content container">
+          <video autoPlay muted loop>
+              {/* <source src="image/video.mp4"> */}
+          </video>
+        <h1>Ashu Dada</h1>
+        <p>Discover the Secrets Written in the Stars.</p>
+      </div>
+    </header>
+  
+}
+
+export default Home;
